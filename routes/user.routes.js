9b6878ab30f3a1b81ce43cfaf44413b294c6ea60
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-18 13:27:48 -0600

    Added create user with POST method.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,32 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const router = express.Router()
 
-exports.module = router
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(bodyParser.json())
+
+const User = require('../models/User.js')
+
+router.post('/', async (req, res) => {
+    const username = req.body.username
+
+    let user
+
+    try {
+        user = new User({ username })
+        await user.save()
+        
+        console.log('User saved into database')
+    } catch {
+        user = await User.findOne({ username })
+
+        console.error('User already are in database')
+    }
+
+    const _id = user._id
+    
+    res.json({ username, _id })
+})
+
+module.exports = router
