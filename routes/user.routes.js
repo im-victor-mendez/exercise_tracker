2d0c49f5404712e4cfe54c9b39afff440ae7165a
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-18 15:27:21 -0600

    Added exercise POST method.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,7 +6,9 @@ const router = express.Router()
 router.use(bodyParser.urlencoded({ extended: true }))
 router.use(bodyParser.json())
 
+/* Schemas */
 const User = require('../models/User.js')
+const Exercise = require('../models/Exercise.js')
 
 router.post('/', async (req, res) => {
     const username = req.body.username
@@ -33,6 +35,47 @@ router.get('/', async (req, res) => {
     const collection = await User.find({})
 
     res.send(collection)
+})
+
+/* POST exercise */
+router.post('/:_id/exercises', async (req, res) => {
+    const _id = req.params._id || req.body[':_id']
+    const username = await User.findById(_id).username
+
+    const { description, duration } = req.body
+
+    if (!_id || !description || !duration) {
+        console.error('Exercise validation to save has been failed')
+        res.json({ 'error': 'Exercise validation to save has been failed' })
+    } else {
+        let { date } = req.body
+
+        if (!date) date = new Date()
+        else date = new Date(date)
+
+        const exercise = new Exercise({
+            description,
+            duration,
+            date
+        })
+
+        try {
+            exercise.save()
+
+            console.log('Exercise saved')
+
+            res.json({
+                username,
+                description,
+                duration,
+                date: date.toDateString(),
+                _id
+            })
+        } catch {
+            console.error('Error saving exercise, please try again')
+            res.json({ 'error': 'Error saving exercise, please try again' })
+        }
+    }
 })
 
 module.exports = router
